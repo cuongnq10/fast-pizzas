89d1533a67d928eaff228b250ba91dccb07e7627
SBC - CuongNQ1 <cuongnq1@bsscommerce.com>
2025-05-28 18:52:28 +0700

    feat: second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Home from './ui/Home';
 import Error from './ui/Error';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
-import CreateOrder, { action as createOrderAction } from './features/order/CreateOrder';
+import CreateOrder, {
+  action as createOrderAction,
+} from './features/order/CreateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
 import AppLayout from './ui/AppLayout';
 
@@ -30,12 +32,12 @@ const router = createBrowserRouter([
       {
         path: '/order/new',
         element: <CreateOrder />,
-        action: createOrderAction
+        action: createOrderAction,
       },
       {
         path: '/order/:orderId',
         element: <Order />,
-        errorElement: <Error />, 
+        errorElement: <Error />,
         loader: orderLoader,
       },
     ],
---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  //   cart: [],
+  cart: [
+    {
+      pizzaId: 12,
+      name: 'Mediterranean',
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+  ],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {},
+    deleteItem(state, action) {},
+    increaseItemQuantiy(state, action) {},
+    decreaseItemQuantiy(state, action) {},
+    clearCart(state, action) {},
+  },
+});
